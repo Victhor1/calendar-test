5b6afd9d7412be319067321bc7312715f23532e5
Victhor Ramirez <victtorramirez24@outlook.es>
2026-08-31 18:49:19 -0600

    some changes

--- lib/main.dart
+++ lib/main.dart
@@ -137,6 +137,7 @@ class _MyHomePageState extends State<MyHomePage> {
                           scrollBackLimit: 4,
                           scrollForwardLimit: 4,
                           scrollDirection: Axis.vertical,
+                          disableDaysWithoutEvents: true,
                         ),
                       ),
                     ),
